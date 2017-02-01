451d2615e55e5a8a828b15d5444d0ac88095514f
Kelechi Chima <kelechi.chima@gmail.com>
2017-02-01 08:22:25 +0000

    Initial controller test

---
+++ simple-rest-api-ws/src/test/java/com/afrosoft/simple/rest/api/controller/SimpleControllerTest.java
@@ -0,0 +1,22 @@
+package com.afrosoft.simple.rest.api.controller;
+
+import org.junit.Test;
+import org.springframework.http.MediaType;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.setup.MockMvcBuilders.standaloneSetup;
+
+public class SimpleControllerTest {
+
+    @Test
+    public void shouldReturnValidJson() throws Exception {
+        standaloneSetup(new SimpleController()).build()
+                .perform(get("/greet").accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType("application/json;charset=UTF-8"))
+                .andExpect(jsonPath("$.message").value("Hello world"));
+    }
+}
